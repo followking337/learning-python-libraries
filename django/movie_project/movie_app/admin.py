a3a5d899021f075a751b91820d6661f01c75f73f
Daniel Folomkin Boldyrev <daniel773@icloud.com>
2022-02-22 11:48:43 +0100

    Create project 'project_udemy'
    -create apps: mainapp, pages;
    -create layout.html, index.html, about.html, styles.css
    -connect templates: views + urls
    -make class Page in models.py

--- django/movie_project/movie_app/admin.py
+++ django/movie_project/movie_app/admin.py
@@ -1,17 +1,20 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Movie
+from django.db.models import QuerySet
 
 # Register your models here.
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'budget', 'rating_status']  # to view more other fields in admin
+    list_display = ['name', 'rating', 'year', 'budget', 'currency', 'rating_status']
+    # to view more other fields in admin
     # __str__ is not working after setting list_display
     # first field is link to that one specific register, so it can´t be editable
-    list_editable = ['rating', 'year', 'budget']  # make fields editable from admin
+    list_editable = ['rating', 'year', 'currency', 'budget']  # make fields editable from admin
     ordering = ['-rating', 'name']  # rating_status can't be used here because is not attribute
     list_per_page = 3
+    actions = ['set_dollars', 'set_euro']  # register of the method
 
     @admin.display(ordering='rating', description='status')  # after decorating this field can be ordered
     def rating_status(self, movie: Movie):  # by default this field is can´t be ordered
@@ -22,5 +25,14 @@ class MovieAdmin(admin.ModelAdmin):
         if movie.rating <= 85:
             return 'Зачет'
         return 'Топчик'
+
+    @admin.action(description='Установить валюту в доллар')
+    def set_dollars(self, request, qs: QuerySet):
+        qs.update(currency=Movie.USD)
+
+    @admin.action(description='Установить валюту в евро')
+    def set_euro(self, request, qs: QuerySet):
+        count_updated = qs.update(currency=Movie.EUR)
+        self.message_user(request, f'Было обновлено {count_updated} записей', messages.ERROR)
 
 # admin.site.register(Movie, MovieAdmin)  # same as decorator above
--- django/movie_project/movie_app/models.py
+++ django/movie_project/movie_app/models.py
@@ -7,11 +7,20 @@ from django.utils.text import slugify
 
 # from movie_app.models import Movie
 class Movie(models.Model):
+    EUR = 'EUR'
+    USD = 'USD'
+    RUB = 'RUB'
+    CURRENCY_CHOICES = [
+        (EUR, 'Euro'),
+        (USD, 'Dollars'),
+        (RUB, 'Rubles')
+    ]
     # id --> will be created automatically by ORM
     name = models.CharField(max_length=40)
     rating = models.IntegerField()  # any of these changes will affect DB structure, so makemigrations is required
     year = models.IntegerField(null=True, blank=True)  # allows null registers in this field
     budget = models.IntegerField(default=1000000)  # default value if not set
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=RUB)  # default is only for new register
     slug = models.SlugField(default='', null=False, db_index=True)  # db_index -> searching in DB is faster
 
     def save(self, *args, **kwargs):
